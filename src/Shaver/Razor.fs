4f8d3d96175b894c0639a0bff12fe2f50cf2fd2c
Roman Provazník <dzoukr@dzoukr.cz>
2015-12-15 16:11:07 +0100

    added tests for partials

--- src/Shaver/Razor.fs
+++ src/Shaver/Razor.fs
@@ -9,13 +9,22 @@ open Suave.Utils
 open Suave.Razor
 open RazorEngine.Templating
 
+/// Opening tag, default value = {{{
+let mutable openTag = "{{{"
 
-let partialEmpty<'a> = 
+/// Closing tag, default value = }}}
+let mutable closeTag = "}}}"
+
+let private getPartialKey name = sprintf "%s%s%s" openTag name closeTag
+
+/// Renders partial content as empty string
+let empty<'a> = 
     fun _ -> 
         async { 
             return String.Empty
         }
 
+/// Renders partial content
 let partial<'a> path (model : 'a) =
     fun r ->
         async {
@@ -25,9 +34,38 @@ let partial<'a> path (model : 'a) =
             return razorService.RunCompile(razorTemplate, cacheKey, typeof<'a>, model)
         }
 
+/// Renders nested content
+let nested<'a> path (model:'a) (partials:(string * 'b) list)  =
+    fun r ->
+        async {
+            let mutable content = String.Empty
+            let! templateContent = partial path model r
+            content <- templateContent
+            for (key, part) in partials do
+                let! partContent = part r
+                content <- content.Replace(getPartialKey(key), partContent)
+                ()
+            return content
+        }
+
+/// Renders webpart with defined HttpCode, template and model
 let page<'a> code path (model : 'a) = 
     fun r ->
         async {
             let! content = partial path model r
+            return! Response.response code (UTF8.bytes content) r
+        }
+
+/// Renders webpart with defined HttpCode, template and model using partial renders
+let masterPage<'a> code path (model:'a) (partials:(string * 'b) list)  =
+    fun r ->
+        async {
+            let mutable content = String.Empty
+            let! templateContent = partial path model r
+            content <- templateContent
+            for (key, part) in partials do
+                let! partContent = part r
+                content <- content.Replace(getPartialKey(key), partContent)
+                ()
             return! Response.response code (UTF8.bytes content) r
         }
--- tests/Shaver.Tests/Razor.fs
+++ tests/Shaver.Tests/Razor.fs
@@ -15,24 +15,36 @@ open Shaver
 let path = System.Uri(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)).AbsolutePath.Replace("/","\\")
 let runWithConfig = runWith { defaultConfig with homeFolder = Some(path) }
 
-type PageModel = { Message : string}
+type ModelMaster = { Master : string }
+type ModelOne = { One : string }
+type ModelTwo = { Two : string }
 
 [<Test>]
-let ``Empty partial should compile to empty string`` () =
-    let f = async {
-        let! content = Razor.partialEmpty null
-        (OK content)
-        |> runWithConfig
-        |> req HttpMethod.GET "/" None
-        |> should equal System.String.Empty
-    }
-    f |> ignore
-    
-[<Test>]
 let ``Single page should compile with model data`` () =
-    Razor.page HTTP_200 "page.html" { Message = "Hello Razor"}
+    Razor.page HTTP_200 "page.html" { Master = "Hello Razor"}
     |> runWithConfig
     |> req HttpMethod.GET "/" None
     |> should equal "<h1>Hello Razor</h1>"
+
+[<Test>]
+let ``Master page using empties should compile with empty sections`` () =
+    [
+        ("SectionOne", Razor.empty);
+        ("SectionTwo", Razor.empty)
+    ] 
+    |> Razor.masterPage HTTP_200 "masterPage.html" { Master = "Hello Master"}
+    |> runWithConfig
+    |> req HttpMethod.GET "/" None
+    |> should equal "Hello Master<br/>"
     
 
+[<Test>]
+let ``Master page using partials should compile with filled sections`` () =
+    [
+        ("SectionOne", Razor.partial "partialOne.html" { One = "Hello One" });
+        ("SectionTwo", Razor.partial "partialTwo.html" { Two = "Hi Two" })
+    ] 
+    |> Razor.masterPage HTTP_200 "masterPage.html" { Master = "Hello Master"}
+    |> runWithConfig
+    |> req HttpMethod.GET "/" None
+    |> should equal "Hello Master<h1>Hello One</h1><br/><h1>Hi Two</h1>"
