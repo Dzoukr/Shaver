c8cfccdb839ec08ed16b01bde1b2aab4f86cbec0
Roman Provazník <dzoukr@dzoukr.cz>
2015-12-15 21:06:21 +0100

    Completed templating including tests

--- tests/Shaver.Tests/Razor.fs
+++ tests/Shaver.Tests/Razor.fs
@@ -16,6 +16,7 @@ let path = System.Uri(System.IO.Path.GetDirectoryName(System.Reflection.Assembly
 let runWithConfig = runWith { defaultConfig with homeFolder = Some(path) }
 
 type ModelMaster = { Master : string }
+type ModelNested = { Nested : string }
 type ModelOne = { One : string }
 type ModelTwo = { Two : string }
 
@@ -48,3 +49,18 @@ let ``Master page using partials should compile with filled sections`` () =
     |> runWithConfig
     |> req HttpMethod.GET "/" None
     |> should equal "Hello Master<h1>Hello One</h1><br/><h1>Hi Two</h1>"
+
+[<Test>]
+let ``Master page using nested master should compile with filled sections`` () =
+    [    
+        ("SectionOne", Razor.partial "partialOne.html" { One = "Hello One" });
+        ("SectionTwo",
+                    [("NestedOne", Razor.partial "partialOne.html" { One = "Hello Nested One" });
+                     ("NestedTwo", Razor.partial "partialTwo.html" { Two = "Hi Nested Two" })
+                    ] |> Razor.nested "nestedMasterPage.html" {Nested = "Hi Nested"});
+    ]
+     
+    |> Razor.masterPage HTTP_200 "masterPage.html" { Master = "Hello Master"}
+    |> runWithConfig
+    |> req HttpMethod.GET "/" None
+    |> should equal "Hello Master<h1>Hello One</h1><br/>Hi Nested<h1>Hello Nested One</h1><hr/><h1>Hi Nested Two</h1>"
